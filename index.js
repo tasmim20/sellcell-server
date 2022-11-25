623e72ca6ec0e48b447ed8ec215ba5f174565c04
Tasmim Rahman <tasmimrahman2000@gmail.com>
2022-11-25 18:08:50 +0600

    products api created

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 
@@ -25,6 +25,13 @@ async function run(){
             const products = await cursor.toArray();
             res.send(products);
         })
+        
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await productCollection.findOne(query);
+            res.send(service);
+        });
     }
     finally{
 
